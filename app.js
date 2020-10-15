4c3db5d109418c6f1c9e51dfca8329f59a8deb78
Thomas Song <ysthomassong@gmail.com>
2020-10-15 14:34:03 -0700

    fully working code

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 //element selectors
 const countriesList = document.querySelector('.countries-list');
-const inputCountryField = document.querySelector('.input-country-field')
-
-//state
-// const countryDataArray = {
-//     name: [],
-//     flags: [],
-// }
+const inputCountryField = document.querySelector('.input-country-field');
+const inputRegionSelect = document.querySelector('.region-select');
+const countriesSection = document.querySelector('.countries-list');
 
 let countryDataArray;
 
@@ -18,12 +14,29 @@ const countryPromise = async() => {
 const getCountryData = async() => {
     const countryResponse = await fetch(`https://restcountries.eu/rest/v2/all`);
     countryDataArray = await countryResponse.json();
-    createCountryCards(countryDataArray)
+    createCountryCards(countryDataArray);
 }
 
 const createCountryCards = (countryData) => {
+
+    countriesList.innerHTML = ``;
     countryData.forEach(country => {
-        //countryDataArray.flags.push(country.flag);
+        let countryNameArray = country.name.split(' ');
+        const countryNameArrayFixed = countryNameArray.map(word => {
+            const charArray = word.split('');
+            charArray.forEach(char => {
+                if(charArray[0] === '(') {
+                    charArray[1] = charArray[1].toUpperCase();
+                } else {
+                    charArray[0] = charArray[0].toUpperCase();
+                }
+            })
+            const charJoin = charArray.join('');
+            return charJoin;
+        });
+        country.name = countryNameArrayFixed.join(' ');
+
+        const countryPop = new Intl.NumberFormat().format(country.population);
 
         const countryDataCard = `
             <li class="country-card">
@@ -31,35 +44,76 @@ const createCountryCards = (countryData) => {
                     <img class="country-card-flag" src="${country.flag}" alt="">
                     <div class="country-card-brief">
                         <h2 class="country-name">${country.name}</h2>
-                        <p class="country-pop">Population: <span>${country.population}</span></p>
+                        <p class="country-pop">Population: <span>${countryPop}</span></p>
                         <p class="country-region">Region: <span>${country.region}</span></p>
                         <p class="country-cap">Capital: <span>${country.capital}</span></p>
                     </div>
                 </a>
             </li>
         `;
-
-        countriesList.insertAdjacentHTML('beforeend', countryDataCard)
-    
+        countriesList.insertAdjacentHTML('beforeend', countryDataCard);
     })
 }
 
-countryPromise()
-
+countryPromise();
 
 const handleCountrySearch = (e) => {
     const searchQuery = e.target.value;
-
-    const countryDataFiltered = countryDataArray.filter(country => country['name'].includes(searchQuery))
     countryDataArray.forEach(country => {
-        console.log(country.name)
-    })
-    // console.log(searchQuery)
-    console.log(countryDataFiltered)
+        country.name = country['name'].toLowerCase();
+    });
+    const countryDataFiltered = countryDataArray.filter(country => {
+        return country['name'].includes(searchQuery);
+    });
+    createCountryCards(countryDataFiltered);
 }
 
+const handleRegionSelect = (e) => {
+    const countryRegionFiltered = countryDataArray.filter(country => {
+        return country.region === e.target.value;
+    });
+    createCountryCards(countryRegionFiltered);
+}
+
+const handleCountryClick = (e) => {
+    //e.preventDefault()
+    const clickedEl = e.target.parentElement;
+
+    if (clickedEl.matches('.country-card-area')) {
+        console.log(clickedEl.children[1].children[0].textContent)
+        const countryName = clickedEl.children[1].children[0].textContent;
+
+        countryDataArray.forEach(country => {
+            if(country.name === countryName) {
+                console.log(country)
+                localStorage.setItem('countryStorage', JSON.stringify(country));
+                localStorage.setItem('countryDataArray', JSON.stringify(countryDataArray))
+                console.log('done')
+            }
+        });
+    } else if(clickedEl.matches('.country-card-brief')) {
+        console.log(clickedEl.parentElement.children[1].children[0].innerText)
+        countryDataArray.forEach(country => {
+            if(country.name === clickedEl.parentElement.children[1].children[0].innerText) {
+                console.log(country)
+                localStorage.setItem('countryStorage', JSON.stringify(country));
+                localStorage.setItem('countryDataArray', JSON.stringify(countryDataArray))
+                console.log('done')
+            }
+        });
+    } else if (clickedEl.matches('p')) {
+        console.log(clickedEl.parentElement.children[0].innerText)
+        countryDataArray.forEach(country => {
+            if(country.name === clickedEl.parentElement.children[0].innerText) {
+                console.log(country)
+                localStorage.setItem('countryStorage', JSON.stringify(country));
+                localStorage.setItem('countryDataArray', JSON.stringify(countryDataArray))
+            }
+        });
+    }
+}
 
 //event listeners
-inputCountryField.addEventListener('change', handleCountrySearch)
-
-//console.log(countryDataArray.flags)
+inputCountryField.addEventListener('change', handleCountrySearch);
+inputRegionSelect.addEventListener('change', handleRegionSelect);
+countriesSection.addEventListener('click', handleCountryClick)
---
+++ countryPage.js
@@ -0,0 +1,98 @@
+let thisCountryData;
+let countryDataArray;
+
+const countryFlag = document.querySelector('.country-info__flag');
+const countryInfoData = document.querySelector('.country-info__data');
+
+const getCountryData = async() => {
+    thisCountryData = JSON.parse(localStorage.getItem('countryStorage'));
+    //console.log(thisCountryData)
+
+    countryDataArray = JSON.parse(localStorage.getItem('countryDataArray'));
+    //console.log(countryDataArray)
+
+}
+
+const getBorderCountries = () => {
+
+    const borderCountryArray = thisCountryData.borders.map(countryCode => {
+        let countryBorderMatch;
+        countryBorderMatch = countryDataArray.filter(country => countryCode === country.alpha3Code ? country.name : '').map(country => {
+            return country.name
+        })
+        //console.log(countryBorderMatch)
+        return countryBorderMatch[0]
+    })
+    //console.log(borderCountryArray)
+    return borderCountryArray;
+}
+
+const insertHTML = () => {
+
+    countryFlag.innerHTML = `
+        <img src="${thisCountryData.flag}" alt="">
+    `;
+
+
+    const thisCountryLanguage = thisCountryData.languages.map(language => {
+        return language.name
+    });
+
+    let languageList = thisCountryLanguage.join(', ');
+
+    const countryPop = new Intl.NumberFormat().format(thisCountryData.population);
+
+    countryInfoData.innerHTML = `
+                <h1 class="country-name">${thisCountryData.name}</h1>
+                <div class="data-section">
+                    <div class="data-section-1">
+                        <p>Native Name: <span class="native-name">${thisCountryData.nativeName}</span></p>
+                        <p>Population: <span class="population">${countryPop}</span></p>
+                        <p>Region: <span class="region">${thisCountryData.region}</span></p>
+                        <p>Sub Region: <span class="sub-region">${thisCountryData.subregion}</span></p>
+                        <p>Capital: <span class="capital">${thisCountryData.capital}</span></p>
+                    </div>
+                    <div class="data-section-2">
+                        <p>Top Level Doamin: <span class="domain">${thisCountryData.topLevelDomain}</span></p>
+                        <p>Currencies: <span class="currencies">${thisCountryData.currencies[0].name}</span></p>
+                        <p>Languages: <span class="languages">${languageList}</span></p>
+                    </div>
+                </div>
+    `;
+
+    const borderCountries = getBorderCountries();
+    //console.log(borderCountries)
+
+    borderCountriesHTML = `
+                <div class="border-countries">
+                    <p>Border Countries: 
+                        <span class="border-countries-list">
+                            
+                        </span>
+                    </p>
+                </div>
+    `
+
+    countryInfoData.insertAdjacentHTML('beforeend', borderCountriesHTML)
+
+    borderCountries.forEach(country => {
+        //console.log(countryInfoData.children[2].children[0].children[0])
+        countryInfoData.children[2].children[0].children[0].insertAdjacentHTML('beforeend', `<a href="./country-info.html" class='button-border'>${country}<a/>`)
+    })
+}
+
+const handleCountryClick = (e) => {
+    //e.preventDefault();
+    const borderCountry = e.target.innerText;
+    countryDataArray.forEach(country => {
+        if(country.name === borderCountry) {
+            localStorage.setItem('countryStorage', JSON.stringify(country));
+        }
+    });
+};
+
+getCountryData();
+
+insertHTML();
+
+countryInfoData.children[2].children[0].children[0].addEventListener('click', handleCountryClick)
